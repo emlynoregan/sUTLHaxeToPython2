7c64bf17041319266e00857a13c6bea6d7f94c97
Emlyn O'Regan <emlynoregan@gmail.com>
2016-12-22 12:49:20 +1030

    auto

--- scripts/runlocal.sh
+++ scripts/runlocal.sh
@@ -13,14 +13,14 @@ git pull origin master  || { echo '011' ; exit 1; }
 #2: Run generate and run tests
 cd "$1" || { echo '020' ; exit 1; }
 /usr/bin/haxe buildtests.hxml || { echo '030' ; exit 1; }
-python "$1/munge_haxe_python3_to_python2.py" < "$1/sUTLHaxePython2/sUTLTests3.py" > "$1/sUTLHaxePython2/sUTLTests2.py" || { echo '040' ; exit 1; }
-rm "$1/sUTLHaxePython2/sUTLTests3.py" || { echo '042' ; exit 1; }
-python "$1/sUTLHaxePython2/sUTLTests2.py" || { echo '045' ; exit 1; }
+python "$1/munge_haxe_python3_to_python2.py" < "$1/sUTLHaxePython2/hxsUTLTests3.py" > "$1/sUTLHaxePython2/hxsUTLTests2.py" || { echo '040' ; exit 1; }
+rm "$1/sUTLHaxePython2/hxsUTLTests3.py" || { echo '042' ; exit 1; }
+python "$1/sUTLHaxePython2/hxsUTLTests2.py" || { echo '045' ; exit 1; }
 
 #3: Generate the python interpreter
 /usr/bin/haxe build.hxml || { echo '050' ; exit 1; }
-python "$1/munge_haxe_python3_to_python2.py" < "$1/sUTLHaxePython2/sUTL3.py" > "$1/sUTLHaxePython2/sUTL2.py" || { echo '055' ; exit 1; }
-rm "$1/sUTLHaxePython2/sUTL3.py" || { echo '057' ; exit 1; }
+python "$1/munge_haxe_python3_to_python2.py" < "$1/sUTLHaxePython2/hxsUTL3.py" > "$1/sUTLHaxePython2/hxsUTL2.py" || { echo '055' ; exit 1; }
+rm "$1/sUTLHaxePython2/hxsUTL3.py" || { echo '057' ; exit 1; }
 
 #4: Push the new interpreter to the repo
 cd "$1/sUTLHaxePython2" || { echo '060' ; exit 1; }
